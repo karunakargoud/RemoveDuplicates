43169b0dcf44be1faff09c64f3d9182cf3dd0f94
karunakargoud <karunakargoud1116@gmail.com>
2024-11-25 12:21:19 +0530

    FirstCommit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoveDuplicates
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input=Console.ReadLine();
+            string result = string.Empty;
+            for(int i = 0; i < input.Length; i++)
+            {
+                if(!result.Contains(input[i]))
+                {
+                    result += input[i];
+                }
+            }
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
